ec1400d38d8a0eb58c9104e333ad8207f995865b
Your Name <tima.groshev@yandex.ru>
2020-12-06 13:03:27 +0300

    added scroll handler

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,33 @@
 let aboutQuantites = document.querySelectorAll(".about__quantity");
+let aboutQuantityValues = [
+  {
+    val: 13,
+  },
+  {
+    val: 14,
+  },
+  {
+    val: 30,
+  },
+];
+let distanceToAbout = $(".about").offset().top - $(window).scrollTop() - $(".about").height();
 
-Array.from(aboutQuantites).forEach((i) => {
-  let num = i.textContent;
+let quantityStart = false;
+$(window).on("scroll", showQuantity);
 
-  anime({
-    targets: i,
-    innerHTML: [0, num],
-    easing: "linear",
-    round: 10, // Will round the animated value to 1 decimal
-  });
-});
+function showQuantity() {
+  if ($(window).scrollTop() >= distanceToAbout) {
+    Array.from(aboutQuantites).forEach((item, i) => {
+      anime({
+        targets: item,
+        innerHTML: [0, aboutQuantityValues[i].val],
+        easing: "linear",
+        round: 10,
+      });
+    });
+    quantityStart = true;
+  }
+  if (quantityStart) {
+    $(window).off("scroll", showQuantity);
+  }
+}
